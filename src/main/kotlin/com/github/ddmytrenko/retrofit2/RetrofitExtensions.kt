f2ce6416953380c31ad57392da2047c7854d258c
Dmytro Dmytrenko <dmytrenko.d@gmail.com>
2019-09-28 13:40:23 +0200

    buildRetrofit() extension for retrofit 2.6.2

--- src/main/kotlin/com/github/ddmytrenko/retrofit2/RetrofitExtensions.kt
+++ src/main/kotlin/com/github/ddmytrenko/retrofit2/RetrofitExtensions.kt
@@ -1,6 +1,16 @@
 package com.github.ddmytrenko.retrofit2
 
 import retrofit2.Retrofit
+import retrofit2.Retrofit.Builder
+
+inline fun buildRetrofit(
+    baseUrl: String,
+    builder: Builder.() -> Unit
+): Retrofit =
+    Builder().apply {
+        baseUrl(baseUrl)
+        builder()
+    }.build()
 
 inline fun <reified T : Any> Retrofit.createService(): T =
     create(T::class.java)
