fa655eec2b429386f1ee6a17e68571ccd5ed6c4b
Dmytro Dmytrenko <dmytrenko.d@gmail.com>
2019-09-30 11:51:24 +0200

    Add `safeBaseUrl` extension

--- src/main/kotlin/com/github/ddmytrenko/retrofit/RetrofitExtensions.kt
+++ src/main/kotlin/com/github/ddmytrenko/retrofit/RetrofitExtensions.kt
@@ -3,12 +3,19 @@ package com.github.ddmytrenko.retrofit
 import retrofit2.Retrofit
 import retrofit2.Retrofit.Builder
 
+private const val URL_POSTFIX = "/"
+
+fun Builder.safeBaseUrl(baseUrl: String): Builder {
+    val postfix = if (baseUrl.endsWith(URL_POSTFIX)) "" else URL_POSTFIX
+    return baseUrl(baseUrl + postfix)
+}
+
 inline fun buildRetrofit(
     baseUrl: String,
     builder: Builder.() -> Unit
 ): Retrofit =
     Builder().apply {
-        baseUrl(baseUrl)
+        safeBaseUrl(baseUrl)
         builder()
     }.build()
 
